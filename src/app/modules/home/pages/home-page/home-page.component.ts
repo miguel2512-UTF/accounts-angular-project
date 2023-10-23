cae227b786bd84cc0ff18fb3ee2ccffbce7b77a3
miguel2512-UTF <miguelangelwilchez1001@gmail.com>
2023-10-22 23:02:47 -0500

    get greeting on home component

--- src/app/modules/home/pages/home-page/home-page.component.ts
+++ src/app/modules/home/pages/home-page/home-page.component.ts
@@ -21,4 +21,18 @@ export class HomePageComponent {
       this.userSession = this.authService.getSession()
     }
   }
+
+  getGreeting() {
+    const currentHour = new Date().getHours()
+    
+    if (currentHour < 12) {
+      return "Good morning"
+    }
+
+    if (currentHour < 18) {
+      return "Good afternoon"
+    }
+    
+    return "Good night"
+  }
 }
